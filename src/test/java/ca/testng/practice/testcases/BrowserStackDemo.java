fe51fc63bfdb33c939a49a0e718dd7f8e7fb1090
ekotliar <elena.kotliar@corusent.com>
2020-05-12 12:18:05 -0400

    push to git

---
+++ src/test/java/ca/testng/practice/testcases/BrowserStackDemo.java
@@ -0,0 +1,43 @@
+package ca.testng.practice.testcases;
+
+import java.net.URL;
+import java.util.List;
+import java.net.MalformedURLException;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+    public class BrowserStackDemo {
+
+        public static String userName = "elena605";
+        public static String accessKey = "J1p5G99KyrpQWLxyN9fb";
+
+        public static void main(String args[]) throws MalformedURLException, InterruptedException {
+            DesiredCapabilities caps = new DesiredCapabilities();
+
+            caps.setCapability("device", "Samsung Galaxy S8 Plus");
+            caps.setCapability("os_version", "7.0");
+            caps.setCapability("project", "My First Project");
+            caps.setCapability("build", "My First Build");
+            caps.setCapability("name", "Bstack-[Java] Sample Test");
+            caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
+            AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                    ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+            searchElement.click();
+            AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                    ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+            insertTextElement.sendKeys("BrowserStack");
+            Thread.sleep(5000);
+            List<AndroidElement> allProductsName = driver.findElementsByClassName("android.widget.TextView");
+            assert(allProductsName.size() > 0);
+            driver.quit();
+        }
+    }
