8c5db619e504f6c195fce1de8681b96214f36637
Jaskarn-Kalsi <jaskarn.kalsi@corusent.com>
2020-07-07 09:21:29 -0400

    Updated Practice project for ios. Refactored for simple understanding.

---
+++ src/test/java/ca/testng/practice/ios/testcases/IOSSingleTest.java
@@ -0,0 +1,48 @@
+package ca.testng.practice.ios.testcases;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.ios.IOSElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author jkalsi - QA_Practice
+ * @since May 05, 2020
+ */
+public class IOSSingleTest extends IOSBaseTest {
+    @Test
+    public void test() throws InterruptedException {
+        String searchTerm = "Hello";
+        IOSElement textButton =
+                (IOSElement) new WebDriverWait(driver, 30)
+                        .until(
+                                ExpectedConditions.elementToBeClickable(
+                                        MobileBy.AccessibilityId("Text Button")
+                                )
+                        );
+        textButton.click();
+
+        IOSElement textInput =
+                (IOSElement) new WebDriverWait(driver, 30)
+                        .until(
+                                ExpectedConditions.elementToBeClickable(
+                                        MobileBy.AccessibilityId("Text Input")
+                                )
+                        );
+        textInput.sendKeys(searchTerm + "\n");
+
+        Thread.sleep(5000);
+
+        IOSElement textOutput =
+                (IOSElement) new WebDriverWait(driver, 30)
+                        .until(
+                                ExpectedConditions.elementToBeClickable(
+                                        MobileBy.AccessibilityId("Text Output")
+                                )
+                        );
+
+        Assert.assertEquals(textOutput.getText(), searchTerm);
+    }
+}
